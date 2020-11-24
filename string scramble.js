e9fef69a57781a1ed822c609102033adee07357d
Revanth Strakz <dr.revanthstrakz@gmail.com>
2020-11-24 10:49:27 +0530

    Create string scramble.js

---
+++ string scramble.js
@@ -0,0 +1,16 @@
+function StringChallenge(str1,str2) { 
+  var arr=str1.split('');
+  for(var i=0; i<str2.length;i++)
+  {
+    var index=arr.indexOf(str2[i]);
+    if(index==-1)
+      return 'false';
+    else
+      arr.splice(index,1);
+  } 
+  return 'true'; 
+
+}
+   
+// keep this function call here 
+console.log(StringChallenge(readline()));
